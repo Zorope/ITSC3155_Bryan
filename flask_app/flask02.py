333007190026f5e237d0a54f2e4394cfda894069
Bryan Xiong <bxiong11@uncc.edu>
2022-10-12 13:08:04 -0400

    flask app #2 - created a route and a view to display a note

--- flask_app/flask02.py
+++ flask_app/flask02.py
@@ -21,6 +21,12 @@ def get_notes():
             2:{'title': 'Second note', 'text': 'this is my second note', 'date': '10-2-2020'}}
     return render_template('notes.html', notes=notes)
 
+@app.route('/notes/<note_id>')
+def get_note(note_id):
+    notes = {1:{'title': 'first note', 'text': 'this is my first note', 'date': '10-1-2020'},
+            2:{'title': 'Second note', 'text': 'this is my second note', 'date': '10-2-2020'}}
+    return render_template('note.html', note=notes[int(note_id)])
+
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
 # To see the web page in your web browser, go to the url,
