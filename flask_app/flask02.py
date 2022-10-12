3d6d8ccdd3b65cab5c882a88d9115b872b5970d3
Bryan Xiong <bxiong11@uncc.edu>
2022-10-12 13:10:56 -0400

    flask app #2 - added a third note

--- flask_app/flask02.py
+++ flask_app/flask02.py
@@ -18,13 +18,15 @@ def index():
 @app.route('/notes')
 def get_notes():
     notes = {1:{'title': 'first note', 'text': 'this is my first note', 'date': '10-1-2020'},
-            2:{'title': 'Second note', 'text': 'this is my second note', 'date': '10-2-2020'}}
+            2:{'title': 'Second note', 'text': 'this is my second note', 'date': '10-2-2020'},
+            3:{'title': 'Third note', 'text': 'this is my third note', 'date': '10-3-2020'}}
     return render_template('notes.html', notes=notes)
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
     notes = {1:{'title': 'first note', 'text': 'this is my first note', 'date': '10-1-2020'},
-            2:{'title': 'Second note', 'text': 'this is my second note', 'date': '10-2-2020'}}
+            2:{'title': 'Second note', 'text': 'this is my second note', 'date': '10-2-2020'},
+            3:{'title': 'Third note', 'text': 'this is my third note', 'date': '10-3-2020'}}
     return render_template('note.html', note=notes[int(note_id)])
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
