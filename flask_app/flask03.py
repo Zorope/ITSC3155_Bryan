e75524fe4b06cf4b5deca9a18920bffc3f63748e
Bryan Xiong <bxiong11@uncc.edu>
2022-10-18 16:37:23 -0400

    flask app #3 - capturing form data in POST request

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -37,7 +37,8 @@ def new_note():
     print('request method is', request.method)
     a_user = {'name': 'Mogli', 'email': 'mogli@uncc.edu'}
     if request.method == 'POST':
-        return '<h1> POST method used for this request </h1>'
+        request_data = request.form
+        return f"data: {request_data} !"
     else:
         return render_template('new.html', user=a_user)
 
