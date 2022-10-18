2a16e5b9ba175fe3e8f55762bc767c9d6d741744
Bryan Xiong <bxiong11@uncc.edu>
2022-10-18 16:33:16 -0400

    flask app #3 - added execution path for POST request note

--- flask_app/flask03.py
+++ flask_app/flask03.py
@@ -4,6 +4,7 @@
 import os                 # os is used to get environment variables IP & PORT
 from flask import Flask   # Flask is the web app that we will customize
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)     # create an app
 
@@ -20,19 +21,25 @@ def get_notes():
     notes = {1:{'title': 'first note', 'text': 'this is my first note', 'date': '10-1-2020'},
             2:{'title': 'Second note', 'text': 'this is my second note', 'date': '10-2-2020'},
             3:{'title': 'Third note', 'text': 'this is my third note', 'date': '10-3-2020'}}
-    return render_template('notes.html', notes=notes)
+    a_user = {'name': 'Bryan', 'email': 'mogli@uncc.edu'}
+    return render_template('notes.html', notes=notes, user=a_user)
 
 @app.route('/notes/<note_id>')
 def get_note(note_id):
     notes = {1:{'title': 'first note', 'text': 'this is my first note', 'date': '10-1-2020'},
             2:{'title': 'Second note', 'text': 'this is my second note', 'date': '10-2-2020'},
             3:{'title': 'Third note', 'text': 'this is my third note', 'date': '10-3-2020'}}
-    return render_template('note.html', note=notes[int(note_id)])
+    a_user = {'name': 'Bryan', 'email': 'mogli@uncc.edu'}
+    return render_template('note.html', note=notes[int(note_id)], user=a_user)
 
-@app.route('/notes/new')
+@app.route('/notes/new', methods=['GET','POST'])
 def new_note():
+    print('request method is', request.method)
     a_user = {'name': 'Mogli', 'email': 'mogli@uncc.edu'}
-    return render_template('new.html', user=a_user)
+    if request.method == 'POST':
+        return '<h1> POST method used for this request </h1>'
+    else:
+        return render_template('new.html', user=a_user)
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=True)
 
